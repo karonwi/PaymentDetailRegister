489f78b27bd23ff034b75c57f12b88dd5e7d303c
karonwi <iyinoluwa@thankucash.com>
2022-07-03 01:12:54 +0100

    first commit

---
+++ PaymentAPI/PaymentAPI/Data/PaymentDetailsContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PaymentAPI.Models;
+
+namespace PaymentAPI.Data
+{
+    public class PaymentDetailsContext :DbContext
+    {
+        public PaymentDetailsContext(DbContextOptions<PaymentDetailsContext>options) : base(options)
+        {
+
+        }
+
+        public DbSet<PaymentDetail> PaymentDetails { get; set; }
+    }
+}
